f5387b4218f2c9cd46dc307dc0566ce566b4303d
Satishnjr <sksatishreddy@gmail.com>
2017-11-29 12:58:25 +0530

    Assertion for footer links

--- src/test/java/com/test/mirchi/modules/FooterPageAction.java
+++ src/test/java/com/test/mirchi/modules/FooterPageAction.java
@@ -9,26 +9,38 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import com.test.mirchi.helpers.Log;
+
 public class FooterPageAction {
-	public static void Execute(WebDriver driver) throws Exception{
+	public static void Execute(WebDriver driver) throws Exception {
 		@SuppressWarnings("unused")
 		WebDriverWait wait = new WebDriverWait(driver, 10);
-		List<WebElement> items = driver.findElements(By.cssSelector(".footer-social-wrap a"));
-		
-		for(WebElement element:items)
-		{
-		    
-		    System.out.println(element.getAttribute("href"));
-		    String actualvalue = "https://twitter.com/Mirchi9";
-		    Assert.assertEquals(actualvalue, "https://twitter.com/Mirchi9");
-		    System.out.println(actualvalue);
+		List<WebElement> items = driver.findElements(By
+				.cssSelector(".footer-social-wrap a"));
+
+		for (WebElement element : items) {
+
+			String actualvalue = element.getAttribute("href");
+			if (actualvalue.contains("twitter")) {
+				Assert.assertEquals(actualvalue, "https://twitter.com/Mirchi9");
+			} else if (actualvalue.contains("facebook")) {
+				Assert.assertEquals(actualvalue,
+						"https://www.facebook.com/mirchi9dotcom");
+
+			} else if (actualvalue.contains("Google+")) {
+				Assert.assertEquals(actualvalue,
+						"https://plus.google.com/+MirchiFans");
+
+			} else if (actualvalue.contains("contact us")) {
+				Assert.assertEquals(actualvalue, "/contact-us/");
+
+			}
+
+			Reporter.log("Found Footer links: ", items.size());
+			Log.info("Found Footer Links");
+			Assert.assertEquals(5, items.size());
+
 		}
-		Reporter.log("Found Footer links: ", items.size());
-		Log.info("Found Footer Links");
-		Assert.assertEquals(5,items.size());
-		
+
 	}
 
 }
-
-
--- src/test/java/com/test/mirchi/step_definitions/RunCukesTest.java
+++ src/test/java/com/test/mirchi/step_definitions/RunCukesTest.java
@@ -5,13 +5,9 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "classpath:features/Footer.feature",
-		plugin = {"pretty", "html:target/cucumber-html-report","json:cucumber.json"},
-		tags = {}
-		)
-public class RunCukesTest{
-	
+@CucumberOptions(features = "classpath:features/Footer.feature", plugin = {
+		"pretty", "html:target/cucumber-html-report", "json:cucumber.json" }, tags = {})
+public class RunCukesTest {
+
 }
