84a04a7204fc6e8b1f51064fa772253a43c3a57c
Satishnjr <sksatishreddy@gmail.com>
2017-12-01 22:16:36 +0530

    removing footer feature in run cukes

--- src/test/java/com/test/mirchi/step_definitions/RunCukesTest.java
+++ src/test/java/com/test/mirchi/step_definitions/RunCukesTest.java
@@ -6,8 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features/Footer.feature", plugin = {
-		"pretty", "html:target/cucumber-html-report", "json:cucumber.json" }, tags = {})
+@CucumberOptions(features = "classpath:features", plugin = { "pretty",
+		"html:target/cucumber-html-report", "json:cucumber.json" }, tags = {})
 public class RunCukesTest {
 
 }
